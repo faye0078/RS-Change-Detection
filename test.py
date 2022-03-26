8a4fb09ffb8310c0b4b0fcc9f0062a14da895207
mick mo <faye892322@gmail.com>
2022-03-27 00:31:26 +0800

    add predicter

--- test.py
+++ test.py
@@ -12,50 +12,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import argparse
 import os
-
 import paddle
+from model.concat_model import get_concat_model
+from model.split_model import get_split_model
+from engine.predicter import Predicter
+from utils.preprocess import make_transform
+from dataloader import make_dataloader
+from utils.yaml import _parse_from_yaml
 from configs.MyConfig import get_test_config
 from configs.config import Config
-from paddleseg.utils import get_sys_env, logger, config_check
-from paddleseg.core import predict
-
-def get_image_list(image_path):
-    """Get image list"""
-    valid_suffix = [
-        '.JPEG', '.jpeg', '.JPG', '.jpg', '.BMP', '.bmp', '.PNG', '.png'
-    ]
-    image_list = []
-    image_dir = None
-    if os.path.isfile(image_path):
-        if os.path.splitext(image_path)[-1] in valid_suffix:
-            image_list.append(image_path)
-        else:
-            image_dir = os.path.dirname(image_path)
-            with open(image_path, 'r') as f:
-                for line in f:
-                    line = line.strip()
-                    if len(line.split()) > 1:
-                        line = line.split()[0]
-                    image_list.append(os.path.join(image_dir, line))
-    elif os.path.isdir(image_path):
-        image_dir = image_path
-        for root, dirs, files in os.walk(image_path):
-            for f in files:
-                if '.ipynb_checkpoints' in root:
-                    continue
-                if os.path.splitext(f)[-1] in valid_suffix:
-                    image_list.append(os.path.join(root, f))
-    else:
-        raise FileNotFoundError(
-            '`--image_path` is not found. it should be an image file or a directory including images'
-        )
-
-    if len(image_list) == 0:
-        raise RuntimeError('There are not image file in `--image_path`')
-
-    return image_list, image_dir
+from paddleseg.utils import get_sys_env, logger
 
 
 def main(args):
@@ -66,42 +33,12 @@ def main(args):
     paddle.set_device(place)
     if not args.cfg:
         raise RuntimeError('No configuration file specified.')
-
-    cfg = Config(args.cfg)
-    val_dataset = cfg.val_dataset
-    if not val_dataset:
-        raise RuntimeError(
-            'The verification dataset is not specified in the configuration file.'
-        )
-
-    msg = '\n---------------Config Information---------------\n'
-    msg += str(cfg)
-    msg += '------------------------------------------------'
-    logger.info(msg)
-
-    model = cfg.model
-    transforms = val_dataset.transforms
-    image_list, image_dir = get_image_list(args.image_path)
-    logger.info('Number of predict images = {}'.format(len(image_list)))
-
-    config_check(cfg, val_dataset=val_dataset)
-
-    predict(
-        model,
-        model_path=args.model_path,
-        transforms=transforms,
-        image_list=image_list,
-        image_dir=image_dir,
+    predicter = Predicter(args)
+    predicter.predict(
+        image_dir=args.image_dir,
         save_dir=args.save_dir,
         aug_pred=args.aug_pred,
-        scales=args.scales,
-        flip_horizontal=args.flip_horizontal,
-        flip_vertical=args.flip_vertical,
-        is_slide=args.is_slide,
-        crop_size=args.crop_size,
-        stride=args.stride,
     )
-
 
 if __name__ == '__main__':
     args = get_test_config()
